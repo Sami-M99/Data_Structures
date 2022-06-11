cb8c5e3120b3249ee83a67edd14a1e43e324f394
Sami.M <samimansour1999131@gmail.com>
2022-06-11 21:08:18 +0300

    Recursion Example

---
+++ Recursion_Factorial_App/Math.cs
@@ -0,0 +1,33 @@
+﻿namespace Recursion_Factorial_App
+{
+    public class Math
+    {
+        // Math.Factorial(int)
+        // Iteration  like for,while...
+        public static int Factorial(int input)
+        {
+            if (input <= 1)
+                return 1;
+
+            int result = 1;
+            int step = 1;
+            while (step <= input)
+            {
+                result *= step;
+                step++;
+            }
+            return result;
+        }
+
+
+        // Recursion
+        public static int FactorialRecursion(int input)
+        {
+            // base case
+            if (input <= 1)
+                return 1;
+            return input * FactorialRecursion(input - 1);
+        }
+    }
+
+}
