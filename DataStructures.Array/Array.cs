a628b7fb1362c9ff5dc3a5f4bb48ae7cc17f10ee
Sami.M <samimansour1999131@gmail.com>
2022-06-11 14:40:48 +0300

    Add interfaces to Array class

--- DataStructures.Array/Array.cs
+++ DataStructures.Array/Array.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace DataStructures.Array
 {
@@ -42,5 +43,93 @@ namespace DataStructures.Array
 
             InnerArray[index] = value;
         }
+
+        /** ICloneable **/
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        /** IEnumerable **/
+        // this to able make a loop(for) in ConsoleApp
+        public IEnumerator GetEnumerator()
+        {
+            /* Print Array from first to last */
+            // return InnerArray.GetEnumerator();
+            return new CustomArrayEnumerator(InnerArray);
+
+            /* Or Print it from last to first */
+            // return  new CustomArrayEnumeratorX(InnerArray);
+        }
+
+        public int IndexOf(Object value)
+        {
+            for (int i = 0; i < InnerArray.Length; i++)
+            {
+                if (GetValue(i).Equals(value))
+                    return i;
+            }
+            return -1; // O(n)
+        }
+
     }
+
+    public class CustomArrayEnumerator : IEnumerator
+    {
+        private Object[] _array;
+        int index;
+
+        public CustomArrayEnumerator(Object[] SourceArray)
+        {
+            _array = SourceArray;
+            index = -1;
+        }
+
+        public object Current => _array[index];
+
+        public bool MoveNext()
+        {
+            if (index < _array.Length - 1) // here -1 because we made index++ for after
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+    }
+
+    public class CustomArrayEnumeratorX : IEnumerator
+    {
+        Object[] _array;
+        int index;
+        public CustomArrayEnumeratorX(Object[] SourceArray)
+        {
+            _array = SourceArray;
+            index = SourceArray.Length;
+        }
+
+        public object Current => _array[index];
+
+        public bool MoveNext()
+        {
+            if (index > 0)
+            {
+                index--;
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            index = _array.Length;
+        }
+
+    }
+
 }
