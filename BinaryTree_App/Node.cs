71b15459a2acfb99bd15b61bba7c658846c61fe5
Sami.M <samimansour1999131@gmail.com>
2022-06-12 01:07:17 +0300

    Binary Tree Class,Tests and ConsoleApp

---
+++ BinaryTree_App/Node.cs
@@ -0,0 +1,19 @@
+﻿namespace BinaryTree_App
+{
+    public class Node<T>
+    {
+        public Node()
+        {
+
+        }
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+    }
+
+}
