0009a09b0ed00859d79ac717b069a29520d35176
Sami.M <samimansour1999131@gmail.com>
2022-06-11 14:26:10 +0300

    Value and Reference Type with Tests

---
+++ ValueAndReferenceType/ReferenceType.cs
@@ -0,0 +1,18 @@
+﻿namespace ValueAndReferenceType
+{
+    public class ReferenceType
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+
+    }
+
+
+    // // Or we can write it like this
+    //public record RecordType
+    //{
+    //    public int X { get; init; } // here we can't make "set"
+    //    public int Y { get; init; } // Because "record" it does't anable change the value 
+    //}
+}
